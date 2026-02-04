175808f8743bf1875748306d4628a2ba4b47f35b
오혜성 <ohs020105@gmail.com>
2026-02-04 21:51:16 +0900

    [Feat] Spring Boot 초기 프로젝트 생성

---
+++ src/main/java/com/example/study_flow_server/StudyFlowServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_flow_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyFlowServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyFlowServerApplication.class, args);
+	}
+
+}
