da5b892191679b90903784e021f83c89a4f240d7
오혜성 <ohs020105@gmail.com>
2026-04-07 18:53:37 +0900

    fix: add default values to all properties to ensure startup

--- src/main/java/com/example/study_flow_server/global/config/S3Config.java
+++ src/main/java/com/example/study_flow_server/global/config/S3Config.java
@@ -11,13 +11,13 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.access-key:}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secret-key:}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${cloud.aws.region.static:ap-northeast-2}")
     private String region;
 
     @Bean
